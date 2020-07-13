62c4eceed728f2b32adf91b613b1ca55ce89bb76
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-07-13 19:58:42 +0300

    Added a basic example of validation for the username field

--- vanilla_js/form_validation/index.js
+++ vanilla_js/form_validation/index.js
@@ -4,7 +4,30 @@ const email = document.getElementById('email')
 const password = document.getElementById('password');
 const password_repeat = document.getElementById('passwrod-repeat');
 
+// General purpose functions
 
+// Display error message on specified input 
+const showError = (input, message) => {
+    const formControl = input.parentElement;
+    const smallTagMessage = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    smallTagMessage.innerText = message;
+}
+
+// Display success message on specified input
+const showSuccess = (input, message) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Event listeners
 form.addEventListener('submit', (e) => {
-    console.log('Hello');
+    e.preventDefault();
+    
+    if (username.value === '') {
+        showError(username, 'The username is not valid');
+    } else {
+        showSuccess(username, 'The username is valid');
+    }
+    
 })
