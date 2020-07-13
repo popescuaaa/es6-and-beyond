b1df284be35b7c045637e6c8c3fec5042b42133f
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-07-13 19:31:08 +0300

    Started form validation made in pure js

---
+++ vanilla_js/form_validation/index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email')
+const password = document.getElementById('password');
+const password_repeat = document.getElementById('passwrod-repeat');
+
+
+form.addEventListener('submit', (e) => {
+    console.log('Hello');
+})
