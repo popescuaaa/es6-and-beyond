ab1d47b7d4ca5e5e22ebfbe4c039a4349e0362d6
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-07-14 19:23:02 +0300

    Finished refactoring the index.js with full functionality for form validation

--- vanilla_js/form_validation/index.js
+++ vanilla_js/form_validation/index.js
@@ -2,11 +2,9 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const repeatedPassword = document.getElementById("passwrod2");
+const repeatedPassword = document.getElementById("repeated-password");
 
-// General purpose functions
-
-// Display error message on specified input
+// Display error message on specified input element
 const showError = (input, message) => {
   const formControl = input.parentElement;
   const smallTagMessage = formControl.querySelector("small");
@@ -20,35 +18,64 @@ const showSuccess = (input) => {
   formControl.className = "form-control success";
 };
 
+const getFiledName = (input) =>
+  input.id.charAt(0).toUpperCase() + input.id.slice(1);
+
 // Validation functions
-const validateEmail = (email) => {
+const checkEmail = (input) => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(String(input.value.toLowerCase()))) {
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not in valid form!`);
+  }
 };
-
-const validatePassword = (password) => {
-    const size = password.value.length;
-    return size > 8 ? true : false;
-}
-
-const validateRepeatedPassword = (password, repeatedPassword) => {
-    const pValue = password.value;
-    const p2Value = repeatedPassword.value;
-    retrun (pValue.trim() ===  p2Value.trim()) ? true : false;
-}
 
 const checkRequiredFiled = (inputArr) => {
   inputArr.forEach((element) => {
     if (element.value.trim() === "") {
-      showError(element, "Required filed!");
+      showError(element, `${getFiledName(element)} is a required filed!`);
     } else {
       showSuccess(element);
     }
   });
 };
 
+const checkLength = (inputArr, minLength, maxLength) => {
+  inputArr.forEach((element) => {
+    if (element.value.length < minLength) {
+       showError(
+        element,
+        `${getFiledName(
+          element
+        )} has to be between ${minLength} and ${maxLength} characters!`
+      );
+    } else if (element.value.length > maxLength) {
+     showError(
+        element,
+        `${getFiledName(
+          element
+        )} has to be between ${minLength} and ${maxLength} characters!`
+      );
+    } else {
+      showSuccess(element);
+    }
+  });
+};
+
+const checkPasswordMatching = (password, repeatedPassword) => {
+  if (password.value.trim() === repeatedPassword.value.trim()) {
+    showSuccess(repeatedPassword);
+  } else {
+    showError(repeatedPassword, "The repeated password don't match");
+  }
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkRequiredFiled([username, password, email, password2]);
+  checkRequiredFiled([username, password, email, repeatedPassword]);
+  checkEmail(email);
+  checkLength([username, password, repeatedPassword], 8, 15);
+  checkPasswordMatching(password, repeatedPassword);
 });
