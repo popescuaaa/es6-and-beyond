1754d1882dbea26093f705e15749e99405b8126c
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-07-19 16:23:51 +0300

    Stucked at retriving articles with Status 426 - Update to HTTP/2.0

--- vanilla_js/news/index.js
+++ vanilla_js/news/index.js
@@ -1,24 +1,44 @@
-const ariclesShowcase = document.getElementById('showcase');
-const searchButton = document.getElementById('search-button');
-const searchBar = document.getElementById('search-bar');
+const articlesShowcase = document.querySelector(".showcase");
+const searchButton = document.getElementById("search-button");
+const searchBar = document.getElementById("search-bar");
 
-const API_KEY = 'aa2197669263422d9d2b829c3fbc797f';
-let searchTopic = '';
+const API_KEY = "aa2197669263422d9d2b829c3fbc797f";
+let searchTopic = "";
+let articles = [];
 
 const getArticles = async (topic) => {
+  const url = `https://newsapi.org/v2/everything?q=bitcoin&from=2020-07-19&sortBy=popularity&apiKey=aa2197669263422d9d2b829c3fbc797f`;
+  const req = new Request(url);
 
-}
+  fetch(req)
+    .then((response) => {
+      console.log("Success!", response);
+    })
+    .catch((err) => {
+      console.warn("Something went wrong.", err);
+    });
+};
 
-const populateUI = () => {
-  
-}
+// Map articles to list elements
+const populateUI = async () => {
+  await getArticles("bitcoin");
+
+  if (articles.length === 0) {
+    let node = document.createElement("LI");
+    let textNode = document.createTextNode("Retriving data...");
+    node.appendChild(textNode);
+    articlesShowcase.appendChild(node);
+  }
+};
 
 // Event listeners
-searchBar.addEventListener('change', (e) => {
+searchBar.addEventListener("change", (e) => {
   e.preventDefault();
   searchTopic = e.target.value;
-})
+});
 
-searchButton.addEventListener('click', (e) => {
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-})
+});
+
+populateUI();
