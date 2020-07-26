da7e9efbcedf1278dab3d608495b9d1e738473e7
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-07-26 11:57:51 +0300

    Cleaned the pong project

--- pong/containers/Algorithms.js
+++
@@ -1,5 +0,0 @@
-/**
-  Place to implement an algorithm for
-  winning continuously -> q learning from scratch maybe
-  - maybe implement a neural net
- **/
--- pong/index.js
+++
@@ -1,5 +0,0 @@
-import { Pong } from "./components/Pong.js";
-
-const canvas = document.getElementById("gamepad");
-const game = new Pong(canvas);
-game.drawGamePad();
